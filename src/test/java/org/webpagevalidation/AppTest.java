be484e33b1d45c59027b8853fa237630ab5a255b
harshitkrverma <harshitkumarverma@gmail.com>
2023-12-31 15:02:38 +0530

    Junit pass

--- src/test/java/org/webpagevalidation/AppTest.java
+++ src/test/java/org/webpagevalidation/AppTest.java
@@ -3,7 +3,6 @@ package org.webpagevalidation;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
